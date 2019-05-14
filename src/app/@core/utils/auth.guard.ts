44449a7b50979c526e1a68ca686832dbeb8e408c
Jose Alvaro Flores Gambarelli <Floresgambarelli@gmail.com>
2019-05-13 19:17:53 -0600

    test commit

--- src/app/@core/utils/auth.guard.ts
+++ src/app/@core/utils/auth.guard.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../../auth.service';
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthGuard implements CanActivate {
 
   constructor(public authService: AuthService, public router: Router){
